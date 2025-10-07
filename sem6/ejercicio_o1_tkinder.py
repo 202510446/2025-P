5c45f28a28f94e27ad4b944a77a5995dc9bf4aba
202510446 <202510446@urp.edu.pe>
2025-10-07 16:49:07 +0000

    sem7

--- sem6/ejercicio_o1_tkinder.py
+++ sem6/ejercicio_o1_tkinder.py
@@ -1,31 +1,28 @@
-""""
-    objetivo: Empezar a trabajar con interfaz de presentacion de ventana
-    fecha: 30-09-2025
-"""
 
 import tkinter as tk
 from tkinter import messagebox
 
-def ralizar_saludo():
+def realizar_saludo():
     dato = nombre.get()
-    dato_largo = texto_largo.get(1.0, tk.END)
-    messagebox.showinfo(f"{dato}", f"{dato_largo}")
-    
+    dato_largo = texto_largo.get('1.0', tk.END) # indicar posicion de inicio y posicion final del texto
+    messagebox.showinfo(f"{dato}", f'{dato_largo}')
+
+
 root = tk.Tk()
-#definir propiedades de la ventana principal
-root.title("Mi primera ventana ")
-root.geometry("300x200") # ancho x altos
+#  Definir prpiedades de la ventana principal
+root.title("Mi primera ventana")
+root.geometry("300x200") # ancho x alto
 
-etiqueta = tk.Label(root, text="bienvenido a Tkinter",font="Arial 14")
-etiqueta.pack() 
+etiqueta = tk.Label(root, text="Bienvenido a Tkinter", font="Arial, 14")
+etiqueta.pack()
 
-nombre= tk.Entry(root)
+nombre = tk.Entry(root)
 nombre.pack()
 
-boton= tk.Button(root, text="Saludo",command=ralizar_saludo)
+boton = tk.Button(root, text='Saludo', command=realizar_saludo)
 boton.pack()
 
 texto_largo = tk.Text(root, height=4, width=30)
 texto_largo.pack()
 
-root.mainloop() # Inicia el loop de la ventana
+root.mainloop() # Inicia el loop de la interfaz
